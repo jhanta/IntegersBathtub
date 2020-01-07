92f951a20d654bc570cf7b559065b3944c10bc6e
jhanta <57496173+jhanta@users.noreply.github.com>
2020-01-06 21:04:56 -0500

    Add files via upload

--- IntegersBathtub/src/IntegerBathtubWindow.java
+++ IntegersBathtub/src/IntegerBathtubWindow.java
@@ -7,7 +7,7 @@ import java.awt.event.MouseAdapter;
 
 public class IntegerBathtubWindow extends JFrame{
 
-    private IntegerCounterComponent myIntegersCounters[] = new IntegerCounterComponent[2];
+    private IntegerCounterComponent myIntegersCounters[] = new IntegerCounterComponent[3];
     private IntegerCounterComponent hitCounter;
     private Point offset;
 
@@ -16,91 +16,20 @@ public class IntegerBathtubWindow extends JFrame{
         super("Integer Bathtub");
 
         myIntegersCounters[0] = new IntegerCounterComponent(Color.red, 0, 0);
+        myIntegersCounters[0].addMouseListenerCounter();
         myIntegersCounters[1] = new IntegerCounterComponent(Color.blue, 10, 0);
-        MouseAdapter handler;
+        myIntegersCounters[1].addMouseListenerCounter();
+        myIntegersCounters[2] = new IntegerCounterComponent(Color.green, 20, 0);
+        myIntegersCounters[2].addMouseListenerCounter();
+        //MouseAdapter handler;
 
         setLayout(null);
         myIntegersCounters[0].setBounds(0, 0, 500, 500);
         add(myIntegersCounters[0]);
         myIntegersCounters[1].setBounds(10, 0, 500, 500);
         add(myIntegersCounters[1]);
-
-        addMouseListener(new MouseAdapter() {
-
-            public void mousePressed(MouseEvent e) {
-                System.out.println("mousePressed");
-                int xmouse = e.getX();
-                int ymouse = e.getY();
-                double xmin;
-                double xmax;
-                double ymin;
-                double ymax;
-                System.out.println("xmouse" + xmouse);
-                System.out.println("ymouse" + ymouse);
-                System.out.println("ymouse - 23 " + (ymouse - 23));
-                ymouse = ymouse - 23;
-                for (IntegerCounterComponent counter : myIntegersCounters) {
-                    System.out.println("counter: " + counter);
-                    System.out.println("counter.getBounds(): " + counter.getBounds());
-                    xmin = counter.getx1();
-                    xmax = xmin + 16;
-                    ymin = counter.gety1();
-                    ymax = ymin + 16;
-                    System.out.printf("xmin: "+xmin);
-                    System.out.println("xmax: "+xmax);
-                    System.out.println("ymin: "+ymin);
-                    System.out.println("ymax: "+ymax);
-                    if (((xmouse >= xmin) && (xmouse <= xmax)) && ((ymouse >= ymin) && (ymouse <= ymax))){
-                        System.out.println("hit on counter: " + counter);
-                        hitCounter = counter;
-                        System.out.println("hitCounter: " + hitCounter);
-                        offset = new Point();
-                        offset.x = xmouse - (int)xmin;
-                        offset.y = ymouse - (int)ymin;
-                        break;
-                    }
-                }
-            } // end mousePressed
-
-            public void mouseReleased(MouseEvent e) {
-                System.out.println("mouseReleased");
-                hitCounter = null;
-            }
-
-
-            public void mouseClicked(MouseEvent e) {
-                System.out.println("mouseClicked");
-            }
-
-            public void mouseEntered(MouseEvent e) {
-                System.out.println("mouseEntered");
-            }
-
-            public void mouseExited(MouseEvent e) {
-                System.out.println("mouseExited");
-            }
-
-
-
-        }); // end MouseAdapter
-
-        addMouseMotionListener(new MouseMotionListener() {
-            public void mouseMoved(MouseEvent e) {
-                //System.out.println("mouseMoved");
-            } //mousemotion listener
-
-            public void mouseDragged(MouseEvent e) { //mousemotion listener
-                //System.out.println("mouseDragged");
-                //System.out.println("Mouse Drag hitCounter: " + hitCounter);
-                //System.out.println("Mouse Drag offset " + offset);
-                if (hitCounter != null) {
-                    Point mp = e.getPoint();;
-                    //hitCounter.setx1y1(mp.x - offset.x,mp.y - offset.y);
-                    hitCounter.setx1y1(mp.x ,mp.y-23);
-                }
-            }
-
-        });
+        myIntegersCounters[2].setBounds(20, 0, 500, 500);
+        add(myIntegersCounters[2]);
 
     }
 
--- IntegersBathtub/src/IntegerCounterComponent.java
+++ IntegersBathtub/src/IntegerCounterComponent.java
@@ -1,14 +1,17 @@
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JComponent;
 //import java.awt.event.*;
 
-public class IntegerCounterComponent extends JComponent{
+public class IntegerCounterComponent extends JComponent {
 
     //int i;
     private Color colorCounters;
     private int x1;
     private int y1;
-    //private boolean doRepaint = true;
+    private Point offset;
 
 
     public IntegerCounterComponent(Color colorCounters, int x1, int y1) {
@@ -17,6 +20,54 @@ public class IntegerCounterComponent extends JComponent{
         this.y1 = y1;
     }
 
+    public void addMouseListenerCounter() {
+
+        System.out.println("add listener on this: "+this);
+
+        addMouseListener(new MouseAdapter() {
+
+            public void mousePressed (MouseEvent e){
+                System.out.println("mousePressed on this: " + this);
+                int xmouse = e.getX();
+                int ymouse = e.getY();
+                System.out.println("xmouse" + xmouse);
+                System.out.println("ymouse" + ymouse);
+                System.out.println("ymouse - 23 " + (ymouse - 23));
+                ymouse = ymouse - 23;
+                offset = new Point();
+                offset.x = xmouse - (int) x1;
+                offset.y = ymouse - (int) y1;
+            } // end mousePressed
+
+            /*public void mouseReleased (MouseEvent e){
+                System.out.println("mouseReleased");
+                //hitCounter = null;
+            }
+
+            public void mouseClicked (MouseEvent e){
+                System.out.println("mouseClicked");
+            }
+
+            public void mouseEntered (MouseEvent e){
+                System.out.println("mouseEntered");
+            }
+
+            public void mouseExited (MouseEvent e){
+                System.out.println("mouseExited");
+            }*/
+
+        }); // end MouseAdapter
+
+        addMouseMotionListener(new MouseMotionListener() {
+            public void mouseMoved (MouseEvent e){ }
+
+            public void mouseDragged (MouseEvent e){
+                Point mp = e.getPoint();
+                setx1y1(mp.x - 8, mp.y - 23);
+            }
+
+        });
+    }
 
     public void paintComponent(Graphics g) {
         update(g);
@@ -24,25 +75,18 @@ public class IntegerCounterComponent extends JComponent{
 
     public void update (Graphics g) {
         // Recover Graphics2D
-        //if (!doRepaint) {
-            //System.out.println("do not repaint with x1:" + x1 + " y1:" + y1);
-            //doRepaint = true;
-       // } else {
-            Graphics2D g2 = (Graphics2D) g;
-            System.out.println("repaint with x1:" + x1 + " y1:" + y1);
-            g2.setColor(colorCounters);
-            g2.setStroke(new BasicStroke(2));
-            g2.drawLine(x1, y1 + 8, x1 + 16, y1 + 8);
-            g2.drawLine(x1 + 8, y1, x1 + 8, y1 + 16);
-            //doRepaint = false;
-        //}
-
+        Graphics2D g2 = (Graphics2D) g;
+        System.out.println("repaint with x1:" + x1 + " y1:" + y1);
+        g2.setColor(colorCounters);
+        g2.setStroke(new BasicStroke(2));
+        g2.drawLine(x1, y1 + 8, x1 + 16, y1 + 8);
+        g2.drawLine(x1 + 8, y1, x1 + 8, y1 + 16);
     }
 
     public void setx1y1(int xnew1, int ynew1){
         x1 = xnew1;
         y1 = ynew1;
-        System.out.println("in set x1y1 with x1:" + x1 + "y1:" + y1 + "calling repaint");
+        System.out.println("in set x1y1 with x1:" + x1 + "y1:" + y1 + "calling repaint on this: "+this);
         repaint();
     }
 
